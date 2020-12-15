6d24b5202d99dc6614361f7ca50587bf1c7e5f24
a95228 <64647914+A95228@users.noreply.github.com>
2020-12-15 03:25:57 -0800

    second commit

--- stock.py
+++ stock.py
@@ -1,13 +1,36 @@
 from dotenv import load_dotenv  #for loading environment variables
 import os
 import requests
+import csv
+import json
 
 # read personal token
 load_dotenv('.env')
 token = os.getenv("TOKEN")
 
-url = 'https://www.gurufocus.com/api/public/user/' + token + '/stock/AAPL/quote'
+#read csv file
+file = 'Book1.csv'
 
-result = requests.get(url)
+with open(file) as symbols:
+    readCSV = csv.reader(symbols)
+    for symbol in readCSV:
+    
+        url = 'https://www.gurufocus.com/api/public/user/' + token + '/stock/' + symbol[0] + '/quote'
 
-print(result.content)
+        # get response json data.
+        result = requests.get(url)
+
+        if result.status_code == requests.codes.ok:
+                
+            result_array = json.loads(result.content.decode('ascii'))
+
+            # extract price
+            price = result_array['price']
+            currency = result_array['currency']
+            print(price)
+            print(currency)
+        
+
+
+
+
